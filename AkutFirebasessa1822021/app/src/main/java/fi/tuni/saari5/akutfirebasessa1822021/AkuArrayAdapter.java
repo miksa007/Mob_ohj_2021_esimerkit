a6363db2f536c512b307a49983cbdb219b26b286
Mika Saari <mika.saari@tuni.fi>
2021-02-18 13:34:46 +0200

    Valmis - ilman json-filee

--- AkutFirebasessa1822021/app/src/main/java/fi/tuni/saari5/akutfirebasessa1822021/AkuArrayAdapter.java
+++ AkutFirebasessa1822021/app/src/main/java/fi/tuni/saari5/akutfirebasessa1822021/AkuArrayAdapter.java
@@ -1,10 +1,41 @@
 package fi.tuni.saari5.akutfirebasessa1822021;
 
-public class AkuArrayAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AkuArrayAdapter extends ArrayAdapter<Aku> {
     private final static String TAG="softa - AkuArrayAdapter";
 
     //TODO constructor
+    public AkuArrayAdapter(Context context, int resource, List<Aku> objects) {
+        super(context, resource, objects);
+        Log.d("nyt", " ollaan taalla adapterin konstruktorissa");
+    }
 
     //TODO getView
     //Rakennetaan yhden rivin näkymä ListView:iin...
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView==null){
+            convertView = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.item_aku, parent, false);
+        }
+        TextView numeroTextView = (TextView) convertView.findViewById(R.id.numeroTextView);
+        TextView nimiTextView = (TextView) convertView.findViewById(R.id.nimiTextView);
+        Aku taskukirja=getItem(position);
+
+        numeroTextView.setVisibility(View.VISIBLE);
+        numeroTextView.setText(""+taskukirja.getKirjanNumero());
+        nimiTextView.setVisibility(View.VISIBLE);
+        nimiTextView.setText(taskukirja.getKirjanNimi());
+
+        return convertView;
+    }
+
 }
--- AkutFirebasessa1822021/app/src/main/java/fi/tuni/saari5/akutfirebasessa1822021/MainActivity.java
+++ AkutFirebasessa1822021/app/src/main/java/fi/tuni/saari5/akutfirebasessa1822021/MainActivity.java
@@ -1,15 +1,25 @@
 package fi.tuni.saari5.akutfirebasessa1822021;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Context context;
 
+    private ArrayAdapter mAkuListAdapter;
+
+    //private FirebaseDatabase mFirebaseDatabase;
+    //private DatabaseReference mAkussDatabaseReference;
+
+    private FirebaseFirestore mFirestore;
+    private CollectionReference akuReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +47,39 @@ public class MainActivity extends AppCompatActivity {
 
         context=this;
 
+        mFirestore = FirebaseFirestore.getInstance();
+        akuReference = mFirestore.collection("torstai_Akut");
+
         nro = findViewById(R.id.editTextNumero);
         nimi = findViewById(R.id.editTextNimi);
         mAkuListView = (ListView) findViewById(R.id.listView);
 
+        kaikkiAkut = new ArrayList<>();
+        muutosKysely();
     }
+
+    public void muutosKysely(){
+        mFirestore.collection("torstai_Akut").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                kaikkiAkut.clear();
+                Log.d(TAG, "nakyman paivitys?");
+                if(queryDocumentSnapshots!=null) {
+                    for (DocumentSnapshot snapshot : queryDocumentSnapshots) {
+                        Aku aku = snapshot.toObject(Aku.class);
+                        kaikkiAkut.add(aku);
+
+                    }
+                    mAkuListAdapter = new AkuArrayAdapter(context, R.layout.item_aku, kaikkiAkut);
+                    mAkuListAdapter.notifyDataSetChanged();
+                    mAkuListView.setAdapter(mAkuListAdapter);
+                }else {
+                    Log.d(TAG, "Querysnapshot on null");
+                }
+            }
+        });
+    }
+
 
     /**
      * Metodi hoitaa kaikki OnClic - tapahtumat kayttoliittymassa
@@ -51,7 +97,8 @@ public class MainActivity extends AppCompatActivity {
                 aku.setKirjanNimi(nimi.getText().toString());
 
                 //TODO DatabaseReferenceen tuupataan yllä luotu aku
-
+                akuReference = mFirestore.collection("torstai_Akut");
+                akuReference.add(aku);
 
                 tyhjenna();
 
